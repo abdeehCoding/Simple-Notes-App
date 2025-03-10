fa4eaf66009c48aaf12cd3bfb38c950c0a7093a7
abdeehCoding <178068708+abdeehCoding@users.noreply.github.com>
2025-03-10 23:34:28 +0100

    Update script.js
    I remade the entire code; took me about 40 minutes.
    Now it should be less spaghetti code.

--- script.js
+++ script.js
@@ -1,81 +1,71 @@
 // elements' declaration
 const form = document.querySelector('form');
 const noteInput = document.querySelector('#note-input');
-const deleteNotesBtn = document.querySelector('#delete-notes-btn');
+const deleteAllNotesBtn = document.querySelector('#delete-all-notes-btn');
 const notesList = document.querySelector('.notes-list');
 
-let notes = JSON.parse(localStorage.getItem('notes')) || [];
+let notesDB = JSON.parse(localStorage.getItem('notesDB')) || [];
 
 
-// DOM's logic
+// DOM
 renderAllNotes();
 
 form.onsubmit = (e) => {
     e.preventDefault();
-    noteInput.value.trim() === '' ? null : newNote(); 
+
+    noteInput.value.trim() === '' ? null : createNewNote();
+    noteInput.value = '';
+}
+
+deleteAllNotesBtn.onclick = () => {
+    notesList.innerHTML = '';
+    updateDB();
 }
 
 notesList.addEventListener('click', (e) => {
-    if (e.target.closest('.delete-current-note-btn')) {
-        e.target.closest('.note-item').remove();
-
-        updateNotesDB();
-    }
-});
-
-deleteNotesBtn.onclick = () => {
-    deleteNotes();
-}
+    e.target.closest('.delete-note-btn') ? e.target.closest('li').remove() : null;
+    updateDB();
+})
 
 
 // functions
 function renderAllNotes() {
-    notesList.innerHTML = null;
-    notes.forEach(note => {
-        renderNoteToDOM(note);
+    notesList.innerHTML = '';
+    notesDB.forEach(note => {
+        renderNote(note);
     });
 }
 
-function newNote() {
-    renderNoteToDOM(noteInput.value);
-
-    updateNotesDB();
-
-    noteInput.value = '';
-}
-
-function updateNotesDB() {
-    notes = [];
-    document.querySelectorAll('.notes-list p[contenteditable="true"]').forEach(note => {
-        notes.push(note.textContent);
-    });
-
-    localStorage.setItem('notes', JSON.stringify(notes));
-}
-
-function renderNoteToDOM(noteText = '') {
+function renderNote(noteText = '[empty param value]') {
     const note = document.createElement('li');
-    const noteTextContainer = document.createElement('p');
-    const deleteCurrentNoteBtn = document.createElement('button');
+    const noteParagraph = document.createElement('p');
+    const deleteNoteBtn = document.createElement('button');
     const deleteNoteIcon = document.createElement('img');
 
-    deleteNoteIcon.src = "img/trash.svg";
+    deleteNoteIcon.src = 'img/trash.svg';
+    
+    deleteNoteBtn.classList.add('delete-note-btn');
+    deleteNoteBtn.appendChild(deleteNoteIcon);
 
-    deleteCurrentNoteBtn.classList.add('delete-current-note-btn');
-    deleteCurrentNoteBtn.appendChild(deleteNoteIcon);
+    noteParagraph.innerText = noteText;
+    noteParagraph.setAttribute('contenteditable', 'true');
+    noteParagraph.classList.add('note-paragraph');
 
-    noteTextContainer.setAttribute('contenteditable', true);
-    noteTextContainer.innerText = noteText;
-
-    note.classList.add('note-item');
-    note.appendChild(noteTextContainer);
-    note.appendChild(deleteCurrentNoteBtn);
-
+    note.appendChild(noteParagraph);
+    note.appendChild(deleteNoteBtn);
     notesList.appendChild(note);
 }
 
-function deleteNotes() {
-    notesList.innerHTML = '';
-    notes = [];
-    updateNotesDB();
+function createNewNote() {
+    renderNote(noteInput.value);
+    updateDB();
+}
+
+function updateDB() {
+    notesDB = [];
+    let notesDBItems = document.querySelectorAll('.notes-list .note-paragraph');
+    notesDBItems.forEach(note => {
+        notesDB.push(note.innerText);
+    });
+    localStorage.setItem('notesDB', JSON.stringify(notesDB));
 }
